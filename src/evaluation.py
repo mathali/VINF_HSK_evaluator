f0a8af6ae44df096cbe5dd81ced70d90bdb8526a
mathali <matej.halinkovic@gmail.com>
2021-10-16 17:17:16 +0200

    dev: Evaluation v2

--- src/evaluation.py
+++ src/evaluation.py
@@ -13,9 +13,7 @@ def main(get_levels):
         articles = get_articles('../data/hskreading_tab.csv', csv=True, delimiter='\t')
         hsk_dict = get_hsk_dict()
 
-        start = time.time()
-        article_difficulties, discarded_articles = ae.evaluate_articles(articles, hsk_dict, '../output/evaluated_hskreading.csv', True)
-        end = time.time()
+        ae.evaluate_articles(articles, hsk_dict, '../output/evaluated_hskreading.csv', True)
 
     df = pd.read_csv('../output/evaluated_hskreading.csv')
     exact = df['Labeled Level'] == df['Evaluated Level']
@@ -23,19 +21,41 @@ def main(get_levels):
     larger = df['Evaluated Level'] > df['Labeled Level']
     smaller = df['Evaluated Level'] < df['Labeled Level']
 
-    exact.value_counts().plot.bar()
-    plt.show()
-    within_one.value_counts().plot.bar()
-    plt.show()
-    larger.value_counts().plot.bar()
-    plt.show()
-    smaller.value_counts().plot.bar()
-    plt.show()
+    fig = plt.figure(figsize=(8, 6))
 
-    print(f"Accuracy: {exact.sum() / len(exact)}")
-    print(f"Accuracy within one level: {within_one.sum() / len(within_one)}")
-    print(f"Evaluated as higher level: {larger.sum() / len(larger)}")
-    print(f"Evaluated as lower level: {smaller.sum() / len(smaller)}")
+    ax = fig.add_subplot(2, 2, 1)
+    plt.pie(exact.value_counts(), labels=['False', 'True'], autopct='%.2f')
+    plt.title('Accuracy')
+
+    ax = fig.add_subplot(2, 2, 2)
+    plt.pie(within_one.value_counts(), labels=['True', 'False'], autopct='%.2f')
+    plt.title('Accuracy within one level')
+
+    ax = fig.add_subplot(2, 2, 3)
+    plt.pie(larger.value_counts(), labels=['False', 'True'], autopct='%.2f')
+    plt.title('Evaluated as higher level')
+
+    ax = fig.add_subplot(2, 2, 4)
+    plt.pie(smaller.value_counts(), labels=['False', 'True'], autopct='%.2f')
+    plt.title('Evaluated as lower level')
+
+    # accuracy = exact.sum() / len(exact)
+    # acc_within_one = within_one.sum() / len(within_one)
+    # higher = larger.sum() / len(larger)
+    # lower = smaller.sum() / len(smaller)
+    #
+    # ax = fig.add_subplot(3, 1, 2)
+    # ax.set_axis_off()
+    # ax.table(cellText=[[f'{accuracy*100:1.1f}%'],
+    #                             [f'{acc_within_one*100:1.1f}%'],
+    #                             [f'{higher*100:1.1f}%'],
+    #                             [f'{lower*100:1.1f}%']],
+    #                   rowLabels=['Accuracy',
+    #                              'Accuracy within one level',
+    #                              'Evaluated as higher level',
+    #                              'Evaluated as lower level']).auto_set_column_width(0)
+
+    plt.show()
 
 
 if __name__ == '__main__':
