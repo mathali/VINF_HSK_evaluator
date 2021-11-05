7242988838ba787bfaf456f72aad691ae7efaa36
mathali <matej.halinkovic@gmail.com>
2021-11-05 19:55:13 +0100

    ref: Restructured pyspark approach

--- src/distributed/ArticleEvaluator.py
+++ src/distributed/ArticleEvaluator.py
@@ -1,12 +1,6 @@
 import time
-import multiprocessing as mp
 import jieba.posseg as pseg
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
-from pyspark.sql import types as t
-import json
-import ast
-import os
+import utils
 
 
 def evaluate(levels):
@@ -39,52 +33,30 @@ def evaluate(levels):
     return current_level
 
 
-def newCols(word):
+def map_levels(word, hsk_map):
     try:
         return hsk_map.value[word]
     except KeyError:
         return -1
 
-os.environ["PYSPARK_PYTHON"]="/home/matej/PycharmProjects/pythonProject/venv/bin/python3"
+
+def main():
+    spark = utils.setup_spark()
+    articles = utils.get_articles(spark)
+    hsk_map = utils.get_hsk_dict(spark)
+
+    # articles = articles.limit(1000)
+
+    out = articles.rdd.map(lambda article: (article['news_id'], [x.word for x in pseg.cut(article['content']) if x.flag not in ['x', 'eng', 'm']])) \
+                      .map(lambda words: (words[0], [map_levels(word, hsk_map) for word in words[1]])) \
+                      .map(lambda levels: (levels[0],evaluate(levels[1])))
+
+    out.toDF().groupBy('_2').count().show()
 
 
-spark = SparkSession.builder.master("local[1]") \
-    .appName("ArticleEvaluator").getOrCreate()
-
-sc = spark.sparkContext
-
-hsk_dict = sc.wholeTextFiles('../../data/hsk.json').map(lambda x:ast.literal_eval(x[1]))\
-                                                   .map(lambda x: json.dumps(x))
-
-hsk_dict = spark.read.json(hsk_dict)
-hsk_dict.printSchema()
-
-articles = spark.read.json('../../data/new2016zh/news2016zh_valid.json')
-articles.printSchema()
-
-hsk_dict = hsk_dict.withColumn("json", f.create_map(["hanzi", "level"]))
-
-extracted_dict = {}
-for x in [zipped for zipped in zip(*hsk_dict.select('json').collect())]:
-    for entry in x:
-        items = list(entry.items())
-        extracted_dict[items[0][0]] = items[0][1]
-
-hsk_map = sc.broadcast(extracted_dict)
-
-
-callnewColsUdf = f.udf(newCols, t.StringType())
-
-
-test = articles.limit(1000)
-
-start = time.time()
-
-out = test.rdd.map(lambda article: (article['news_id'], [x.word for x in pseg.cut(article['content']) if x.flag not in ['x', 'eng', 'm']])) \
-                  .map(lambda words: (words[0], [newCols(word) for word in words[1]])) \
-                  .map(lambda levels: (levels[0],evaluate(levels[1])))
-
-out.toDF().groupBy('_2').count().show()
-end = time.time()
-print(f'Duration: {(end-start)/60} min')
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    print(f'Duration: {(end-start)/60} min')
 
